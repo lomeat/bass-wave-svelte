0219ab3df22f393603f4bdb63314831175f1b17a
lomeat <lom3at@gmail.com>
2020-07-05 11:28:42 +0300

    add upload, download image with draw canvas

--- src/App.svelte
+++ src/App.svelte
@@ -9,9 +9,66 @@
   let label = "experimental";
   let file = example;
   let fileUrl = null;
+
+  const uploadImage = e => {
+    const reader = new FileReader();
+    const newFile = e.target.files[0];
+
+    reader.onloadend = () => {
+      file = newFile;
+      fileUrl = reader.result;
+    };
+
+    reader.readAsDataURL(newFile);
+  };
+
+  const downloadImage = () => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    canvas.width = 1920;
+    canvas.height = 1080;
+
+    const backImg = new Image();
+    backImg.src = fileUrl ? fileUrl : file;
+    backImg.onload = () => {
+      ctx.drawImage(backImg, 0, 0, canvas.width, canvas.height);
+
+      ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+      ctx.fillRect(0, 934, 1920, 166);
+
+      ctx.fillStyle = "white";
+      ctx.font = "bold 90px HN";
+      ctx.fillText(label.toUpperCase(), 32, 1040);
+    };
+
+    const iconImg = new Image();
+    iconImg.src = icon2x;
+    iconImg.onload = () => {
+      ctx.drawImage(iconImg, 1560, 890, 351, 237);
+
+      const link = document.createElement("a");
+      link.download = "bass-wave.jpg";
+      link.href = canvas.toDataURL("image/jpg");
+      link.click();
+    };
+  };
 </script>
 
-<Input placeholder="Hello" bind:value={label} />
+<Input placeholder="Label name..." bind:value={label} />
+{#if fileUrl}
+  <img style="width: 200px;" src={fileUrl} />
+{:else}
+  <img src={file} style="width: 200px;" />
+{/if}
+<label>
+  Upload
+  <input
+    style="display: none;"
+    accept="image/*"
+    type="file"
+    on:change|preventDefault={e => uploadImage(e)} />
+</label>
+<button on:click={downloadImage}>Download</button>
 
 <style lang="scss">
   @font-face {
